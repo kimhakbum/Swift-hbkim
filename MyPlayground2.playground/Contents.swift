1bc3aaf2f2c429919387221fc50397ddcdaa59d5
Hakbum Kim <rlagkrqj@nave.com>
2017-08-10 15:31:16 +0900

    7차 수정

--- MyPlayground2.playground/Contents.swift
+++ MyPlayground2.playground/Contents.swift
@@ -93,12 +93,59 @@ var circle = Circle(x: 10, y: 10, width: 100, height: 100, color:"white")
 //moveRectangle.draw()
 //moveRectangle.move(toX: 10, toY: 10)
 
+//
+//var tmp:Shape
+//
+//tmp = rectangle
+//tmp.draw()
+//tmp = circle
+//tmp.draw()
 
-var tmp:Shape
 
-tmp = rectangle
-tmp.draw()
-tmp = circle
-tmp.draw()
+
+
+enum CharacterAction{
+    
+    
+    case idle
+    case walk(Int)
+    case run(Int)
+    case jump(Int)
+}
+
+
+
+
+class Character {
+    
+    let name:String
+    var action:CharacterAction
+    
+    
+    init (name:String)
+    {
+        self.name = name
+        self.action = CharacterAction.idle
+        
+    }
+    
+    
+    
+    func run(speed:Int){
+        self.action = CharacterAction.run(speed)
+        switch self.action {
+        case let .run(value) where value >= 120:
+            print("현재 시속 \(value)km. 과속으로 달리는 중입니다.")
+        case let .run(value) where value < 120:
+            print("현재 시속 \(value)km. 정상 속도 입니다.")
+        default :
+            print("인식불가")
+            
+        }
+    }
+}
+
+let character = Character(name:"홍길동")
+character.run(speed:120)
 
 
